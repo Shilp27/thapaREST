aab821b3d63038cdb71f5466c8bf573cb11af309
Shilp <shilpkishor27@gmail.com>
2023-04-04 15:17:10 +0530

    model and schema created and exported, not yet made in database

--- db/connect.js
+++ db/connect.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose')
 
-// uri =
-//   'mongodb+srv://restapi:LFc7wviHdfINBm5M@cluster0.6wjtmda.mongodb.net/?retryWrites=true&w=majority'
-
 const connectDB = (uri) => {
   console.log('connecting to db')
   return mongoose.connect(uri, {
---
+++ models/product.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: [true, 'price must be provided'],
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+  rating: {
+    type: Number,
+    default: 4.8,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  company: {
+    type: String,
+    enum: {
+      value: ['apple', 'samsung', 'dell', 'mi'], // here we don't want company other than these
+      message: `{VALUE} is not supported`,
+    },
+  },
+})
+
+module.exports = mongoose.model('Product', productSchema) // first parameter always write in Singular
