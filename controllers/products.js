c608dd623922b6202f27382d2cdd9294fc72b75c
Shilp <shilpkishor27@gmail.com>
2023-04-04 16:44:06 +0530

    Proper sort functionality added

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product')
 
 const getAllProducts = async (req, res) => {
-  const { company, name } = req.query
+  const { company, name, featured, sort } = req.query
   const queryObject = {}
 
   if (company) {
@@ -21,12 +21,35 @@ const getAllProducts = async (req, res) => {
     iphone only returned iphone before
     but now it returns iphone12 also
   */
-  const myData = await Product.find(queryObject)
+
+  let apiData = Product.find(queryObject)
+
+  if (sort) {
+    let sortFix = sort.replace(',', ' ')
+    apiData = apiData.sort(sortFix)
+  }
+  /* 
+    when user uses sort in URL, the req is with ,
+    eg. /products?sort=name,price
+
+    so we have to remove ,
+
+    also
+    const myData = await Product.find(queryObject).sort(sort)
+    this will apply sort everytime even if user does not request so we use apiData
+  */
+  const myData = await apiData
   res.status(200).json({ myData })
 }
 
 const getAllProductsTesting = async (req, res) => {
-  const myData = await Product.find(req.query) // using req.query get /api/testing?company=mi&user=hello
+  const myData = await Product.find(req.query).sort('-name')
+  /* 
+    mongoose sort three ways
+    query.sort({field:'asc', otherField:-1}) // default asc, can use desc or -1
+    query.sort('field -otherField) // - means desc
+    query.sort([['field','asc']]) // array way of key value pairs
+  */
   res.status(200).json({ myData })
 }
 
